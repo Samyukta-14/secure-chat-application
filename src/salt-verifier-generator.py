435051860017362ac7f878d8158111c02c4baa0b
Samyukta <81011736+Samyukta-14@users.noreply.github.com>
2025-04-24 11:49:55 -0400

    Delete src/salt-verifier-generator.py

--- src/salt-verifier-generator.py
+++
@@ -1,5 +0,0 @@
-import srp
-
-salt, vkey = srp.create_salted_verification_key('alice', '@lice_$', hash_alg=srp.SHA256, ng_type=srp.NG_2048)
-print("Salt:", salt.hex())
-print("Verifier:", vkey.hex())
